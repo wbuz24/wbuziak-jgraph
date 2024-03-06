af19e5e2d22549986028539643c3b5787043e802
wbuz24 <wbuziak@vols.utk.edu>
2024-03-06 18:47:22 -0500

    script update

--- scripts/motd.sh
+++ scripts/motd.sh
@@ -1,9 +1,33 @@
+# if the jgr, jpg and bin directories are not present, make them
+if [ -d bin/ ]; then
+	echo 'bin exists' 1> /dev/null
+else
+	mkdir bin 1> /dev/null
+
+fi
+
+if [ -d jgr/ ]; then
+	echo 'jgr exists' 1> /dev/null
+else
+	mkdir jgr 1> /dev/null
+
+fi
+
+if [ -d jpg/ ]; then
+	echo 'jpg exists' 1> /dev/null
+else
+	mkdir jpg 1> /dev/null
+
+fi
+
+
 # make clean, but shove output to /dev/null
 if [ -f bin/motd ]; then
 	make clean 1> /dev/null
 
 fi
 make 1> /dev/null
+
 
 # output text and run the script
 echo "Music of the Day Jgraph Generator"
@@ -12,3 +36,5 @@ echo "Music of the Day Jgraph Generator"
 # run and convert the jgraph
 `~/Applications/jgraph/jgraph -P jgr/Music-Of-The-Day.jgr | ps2pdf - | convert -density 300 - -quality 100 jpg/Music-Of-The-Day.jpg`
 # `jgraph -P Music-Of-The-Day.jgr | ps2pdf - | convert -density 300 - -quality 100 Music-Of-The-Day.jpg`
+
+echo "Generated jgr/Music-Of-The-Day.jgr and converted to jpg/Music-Of-The-Day.jpg"
